faafb80b97a839217a4960d2e2066a82ca2e08e7
SimoneFranzoni <simofranzoni99@gmail.com>
2022-02-16 17:26:05 +0100

    creazione-home-video

---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <video autoplay muted loop id="myVideo" >
+      <source src="../assets/video.mp4" type="video/mp4">
+    </video>
+    <div class="text">
+      <h1 class="">WELCOME</h1>
+    </div>  
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+  
+  },
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.container{
+  padding: 0px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+
+.text{
+  position: absolute;
+}
+
+h1{
+  font-size: 80px;
+}
+
+#myVideo {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  margin: 0%;
+}
+
+</style>
