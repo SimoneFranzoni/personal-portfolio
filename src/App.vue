03fe7ca31d76db6d9fd010d54afe0ebcf16d9108
SimoneFranzoni <simofranzoni99@gmail.com>
2022-02-17 15:08:11 +0100

    aggiunta bottoni social

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,21 @@
         <source src="./assets/video.mp4" type="video/mp4">
       </video>
       <div class="text">
-        <h1 class="">I am Simone</h1>
-        <h2 class="pb-4">Full-stack Developer</h2>
+        <h1 class="">I am <span>Simone</span></h1>
+        <h2 class="pb-4"> <span>Full-stack</span> Developer</h2>
         <a class="my-btn">Project</a>
-        <a class="my-btn" href="Cv.html" >Cv</a>
+        <a class="my-btn" id="my-btn-orange" href="Cv.html" >Cv</a>
         <a class="my-btn">About me</a>
-      </div>    
+      </div>  
+      <div class="social-btn">
+        <a href="#" class="social-buttons__button social-button social-button--linkedin" aria-label="LinkedIn">
+          <i class="fab fa-linkedin-in"></i>
+        </a>
+        <a href="#" class="social-buttons__button social-button social-button--github" aria-label="GitHub">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+              
     </div>
   </div>  
 </template>
@@ -27,6 +36,12 @@ export default {
 
 <style lang="scss">
 
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
 
 .container-fluid{  
   padding: 0px;
@@ -41,6 +56,11 @@ export default {
 
 .text{
   position: absolute;
+
+  span{
+    color: #ff5100ec;
+  }
+
 }
 
 h1{
@@ -69,7 +89,109 @@ h1{
     &:hover{
       background-color: white;
       color: #272b34;
+      font-size: 20px;
+      font-weight: bold;
     }
 }
+
+#my-btn-orange{
+  background-color: #ff5100ec;
+  border: 1px solid #ff5100ec;
+
+  &:hover{
+    background-color: white;
+    color: #272b34;
+    font-size: 20px;
+    font-weight: bold;
+    border: 1px solid white;
+  }
+}
+
+//social btn
+.social-btn{
+  position: absolute;
+  left: 93%;
+  a{
+    margin: 15px 0;
+  }
+}
+
+$social-button-size: 70px;
+$social-button-icon-size: 0.4;
+$social-button-background: #fff;
+$social-button-active-color: #fff;
+$social-button-transition-time: 0.3s;
+
+$social-button-colors: (
+  'linkedin': #0077b5,
+  'github': #6e5494,
+);
+
+.social-button {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: none;
+  width: $social-button-size;
+  height: $social-button-size;
+  text-decoration: none;
+  border-radius: 100%;
+  text-align: center;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: -1px;
+    left: 50%;
+    display: block;
+    width: 0;
+    height: 0;
+    border-radius: 100%;
+    transition: $social-button-transition-time;
+  }
+
+  &:focus,
+  &:hover {
+    color: black;
+
+    &::after {
+      width: calc(100% + 2px);
+      height: calc(100% + 2px);
+      margin-left: calc(-50% - 1px);
+    }
+  }
+
+  i,
+  svg {
+    position: relative;
+    z-index: 1;
+    transition: $social-button-transition-time;
+  }
+
+  i {
+    font-size: $social-button-size * $social-button-icon-size;
+  }
+
+  svg {
+    height: percentage($social-button-icon-size);
+    width: percentage($social-button-icon-size);
+  }
+
+  @each $name, $color in $social-button-colors {
+    &--#{$name} {
+      color: white;
+
+      &::after {
+        background: white;
+      }
+    }
+  }
+
+}
+//play-btn
+
+
+
 
 </style>
