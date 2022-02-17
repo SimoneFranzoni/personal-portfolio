30134d1580f06cca0b0b58a42a396c27a7929ed7
SimoneFranzoni <simofranzoni99@gmail.com>
2022-02-17 14:21:55 +0100

    Aggiunta link a cv

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,34 @@
 <template>
   <div id="app">
-    <div class="container">
+    <div class="container-fluid">
       <video autoplay muted loop id="myVideo" >
         <source src="./assets/video.mp4" type="video/mp4">
       </video>
       <div class="text">
         <h1 class="">I am Simone</h1>
-        <h2>Full-stack Developer</h2>
-        <button type="button" class="my-btn"> Project</button>
-        <button type="button" class="my-btn"> <a href="Cv.html">Cv</a></button>
-        <button type="button" class="my-btn">About me</button>
+        <h2 class="pb-4">Full-stack Developer</h2>
+        <a class="my-btn">Project</a>
+        <a class="my-btn" href="Cv.html" >Cv</a>
+        <a class="my-btn">About me</a>
       </div>    
     </div>
-    <Cv />
   </div>  
 </template>
 
 <script>
 
-import Cv from './components/Cv.vue'
-
 export default {
   name: 'App',
   components: {
-    Cv,
+    
   }
 }
 </script>
 
 <style lang="scss">
 
-*{
-  background-color: black;
-}
 
-.container{  
+.container-fluid{  
   padding: 0px;
   width: 100%;
   height: 100vh;
@@ -42,7 +36,7 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-  color: rgb(255, 115, 0);
+  color: white;
 }
 
 .text{
@@ -54,54 +48,28 @@ h1{
 }
 
 #myVideo {
-  position: fixed;
   right: 0;
   bottom: 0;
   min-width: 100%;
-  min-height: 100%;
+  min-height: 100vh;
   margin: 0%;
 }
 
 //BUTTON
-button {
- padding: 0.8em 1.8em;
- border: 2px solid rgb(255, 115, 0);
- background: white;
- margin: 10px;
- border-radius: 50px;
- position: relative;
- overflow: hidden;
- background-color: transparent;
- text-align: center;
- text-transform: uppercase;
- font-size: 16px;
- transition: .3s;
- z-index: 1;
- font-family: inherit;
- color: rgb(255, 115, 0);
-}
+.my-btn{
+    color: white;
+    border: 1px solid white;
+    border-radius: 1px;
+    padding: 10px 20px;
+    margin: 10px;
+    text-transform: uppercase;
+    text-decoration: none;
+    transition: .4s;
 
-button::before {
- content: '';
- width: 0;
- height: 300%;
- position: absolute;
- top: 50%;
- left: 50%;
- transform: translate(-50%, -50%) rotate(45deg);
- background: rgb(255, 115, 0);
- transition: .5s ease;
- display: block;
- z-index: -1;
+    &:hover{
+      background-color: white;
+      color: #272b34;
+    }
 }
-
-button:hover::before {
- width: 105%;
-}
-
-button:hover {
- color: black;
-}
-
 
 </style>
